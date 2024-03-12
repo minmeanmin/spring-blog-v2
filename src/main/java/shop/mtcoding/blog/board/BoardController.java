7a7f348626d12558978ffb1006505f0332447c87
minmeanmin <wed1604@naver.com>
2024-03-12 16:11:21 +0900

    글 상세보기+수정하기+삭제하기

--- src/main/java/shop/mtcoding/blog/board/BoardController.java
+++ src/main/java/shop/mtcoding/blog/board/BoardController.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -15,6 +16,26 @@ import java.util.List;
 public class BoardController {
 
     private final BoardNativeRepository boardNativeRepository;
+
+    @PostMapping("/board/{id}/update")
+    public String update(@PathVariable Integer id, String title, String content, String username){
+        boardNativeRepository.updateById(id, title, content, username);
+        return "redirect:/board/"+id;
+    }
+
+    @GetMapping("/board/{id}/update-form")
+    public String updateForm(@PathVariable Integer id, HttpServletRequest request){
+        Board board = boardNativeRepository.findById(id);
+        request.setAttribute("board", board);
+
+        return "board/update-form";
+    }
+
+    @PostMapping("/board/{id}/delete")
+    public String delete(@PathVariable Integer id){
+        boardNativeRepository.deleteById(id);
+        return "redirect:/";
+    }
 
     @PostMapping("/board/save")
     public String save(String title, String content, String username){
@@ -38,7 +59,10 @@ public class BoardController {
     }
 
     @GetMapping("/board/{id}")
-    public String detail(@PathVariable Integer id) {
+    public String detail(@PathVariable Integer id, HttpServletRequest request) {
+        Board board = boardNativeRepository.findById(id);
+        request.setAttribute("board", board);
+
         return "board/detail";
     }
 }
--- src/test/java/shop/mtcoding/blog/board/BoardNativeRepositoryTest.java
+++ src/test/java/shop/mtcoding/blog/board/BoardNativeRepositoryTest.java
@@ -8,12 +8,33 @@ import org.springframework.context.annotation.Import;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @Import(BoardNativeRepository.class)
 @DataJpaTest
 public class BoardNativeRepositoryTest {
 
     @Autowired // IOC에 있는걸 DI 해주는 역할
     private BoardNativeRepository boardNativeRepository;
+
+    @Test
+    public void updateById_test(){
+        //given
+        int id = 1;
+        String title = "제목수정1";
+        String content = "내용수정1";
+        String username = "bori";
+
+        //when
+        boardNativeRepository.updateById(id, title, content, username);
+
+        //then
+        Board board = boardNativeRepository.findById(id);
+        System.out.println("updateById_test/board: " + board);
+        assertThat(board.getTitle()).isEqualTo("제목수정1");
+        assertThat(board.getContent()).isEqualTo("내용수정1");
+        assertThat(board.getUsername()).isEqualTo("bori");
+    }
 
     @Test
     public void finadAll_test(){
@@ -27,8 +48,33 @@ public class BoardNativeRepositoryTest {
         System.out.println("findAll_test/username : " + boardList.get(2).getUsername());
 
         //org.assertj.core.api
-        Assertions.assertThat(boardList.size()).isEqualTo(4);
-        Assertions.assertThat(boardList.get(2).getUsername()).isEqualTo("ssar");
+        assertThat(boardList.size()).isEqualTo(4);
+        assertThat(boardList.get(2).getUsername()).isEqualTo("ssar");
+    }
+    @Test
+    public void deleteById_test(){
+        //given
+        int id = 1;
+
+        //when
+        boardNativeRepository.deleteById(id);
+
+        //then
+        List<Board> boardList = boardNativeRepository.findAll();
+        assertThat(boardList.size()).isEqualTo(3);
     }
 
+    @Test
+    public void findById_test(){
+        //given
+        int id = 1;
+
+        //when
+        Board board = boardNativeRepository.findById(id);
+        //System.out.println("findById_test"+board);
+
+        //then
+        assertThat(board.getTitle()).isEqualTo("제목1");
+        assertThat(board.getContent()).isEqualTo("내용1");
+    }
 }
